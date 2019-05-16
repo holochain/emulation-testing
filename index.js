d4d861324be93d305def0febcaea9325a76330db
Connor Turland <connorturland@gmail.com>
2019-05-16 14:18:02 -0700

    refactor spawnConductor

--- index.js
+++ index.js
@@ -44,47 +44,55 @@ n3h_persistence_path = "${tmpPath}"
 
 
 
-const spawnConductors = (numberOfConductors) => new Promise(resolve => {
-  const conductors = []
-  let starts = 0
-  for (let i = 0; i < numberOfConductors; i++) {
-    const tmpPath = fs.mkdtempSync(path.join(os.tmpdir(), 'n3h-test-conductors-'))
-    const n3hPath = path.join(tmpPath, 'n3h-storage')
-    fs.mkdirSync(n3hPath)
-    const configPath = path.join(tmpPath, `empty-conductor-${i}.toml`)
+const startConductor = i => {
+  const tmpPath = fs.mkdtempSync(path.join(os.tmpdir(), 'n3h-test-conductors-'))
+  const n3hPath = path.join(tmpPath, 'n3h-storage')
+  fs.mkdirSync(n3hPath)
+  const configPath = path.join(tmpPath, `empty-conductor-${i}.toml`)
 
-    const { config, adminPort, instancePort } = genConfig(i, n3hPath)
+  const { config, adminPort, instancePort } = genConfig(i, n3hPath)
 
-    fs.writeFileSync(configPath, config)
+  fs.writeFileSync(configPath, config)
 
-    console.info("Spawning conductor processes...")
+  console.info(`Spawning conductor${i} process...`)
 
-    const handle = child_process.spawn(`holochain`, ['-c', configPath])
+  const handle = child_process.spawn(`holochain`, ['-c', configPath])
 
+  handle.stdout.on('data', data => console.log(`[C${i}]`, data.toString('utf8')))
+  handle.stderr.on('data', data => console.error(`!C${i}!`, data.toString('utf8')))
+  handle.on('close', code => console.log(`conductor ${i} exited with code`, code))
+
+  console.info(`Conductor${i} process spawning successful`)
+
+  return new Promise((resolve) => {
     handle.stdout.on('data', data => {
-      const line = data.toString('utf8')
-      console.log(`[C${i}]`, line)
       // wait for the logs to convey that the interfaces have started
       // because the consumer of this function needs those interfaces
       // to be started so that it can initiate, and form,
       // the websocket connections
-      if (line.indexOf('Starting interfaces...') >= 0) {
-        starts += 1
-        if (starts === numberOfConductors) {
-          resolve(conductors)
-        }
+      if (data.toString('utf8').indexOf('Starting interfaces...') >= 0) {
+        resolve({
+          adminPort,
+          instancePort,
+          handle
+        })
       }
     })
-    handle.stderr.on('data', data => console.error(`!C${i}!`, data.toString('utf8')))
-    handle.on('close', code => console.log(`conductor ${i} exited with code`, code))
+  })
+}
 
-    console.info("Conductor process spawning successful")
 
-    conductors.push({
-      adminPort,
-      instancePort,
-      handle
-    })
+const spawnConductors = async (numberOfConductors) => {
+  const promises = []
+
+  // start the first conductor and
+  // wait for it, because it sets up n3h
+  const firstConductor = await startConductor(0)
+  promises.push(firstConductor)
+
+  for (let i = 1; i < numberOfConductors; i++) {
+    promises.push(startConductor(i))
   }
-})
+  return Promise.all(promises)
+}
 module.exports.spawnConductors = spawnConductors
